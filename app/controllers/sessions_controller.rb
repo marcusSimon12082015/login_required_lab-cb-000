e0b87cd001901a92d793f3e60f6f8eeae49fdad5
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-30 10:54:54 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,11 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create 
-    if current_user.nil? || current_user.empty?
+  def create
+    if current_user.nil? || params[:name].empty
       redirect_to sessions_new_path
-    end 
-  end 
+    else
+      session[:name] = params[:name]  
+    end
+  end
 end
