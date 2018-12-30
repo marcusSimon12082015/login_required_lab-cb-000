4ad5f434e7e5168297bed6c390ecbf621de6c60c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-30 10:57:34 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get 'secrets/show'
 
+  post 'sessions/create'
+  post 'sessions/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
