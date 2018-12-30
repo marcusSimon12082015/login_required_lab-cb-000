7bb367ad8956fbc7146237911a16d19413ee46c2
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-30 10:53:58 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create 
+    if current_user.nil? || current_user.empty?
+      redirect_to sessions_new_path
+    end 
+  end 
 end
